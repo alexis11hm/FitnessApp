3645a080f3f60290a07f69914bfe93c7d0e52f84
Alexis Hernandez <alexis11hm@gmail.com>
2020-06-26 11:05:24 -0500

    initial commit from Create React App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*const element = document.createElement("h1");
+element.innerHTML = "Hello React";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById("root");
+container.appendChild(element);*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const user = {
+  firstname: "Alexis",
+  lastname: "Hernandez",
+  avatar:
+    "https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg",
+};
+
+function getName(user) {
+  return `${user.firstname} ${user.lastname}`;
+}
+
+function getGetting(user) {
+  if (user) {
+    return <h1>Hello React, welcome {getName(user)}</h1>;
+  } else {
+    return <h1>Hello Stranger!</h1>;
+  }
+}
+
+const name = "Alexis Hernandez Mondragon";
+
+const element = (
+  <div>
+    <h1>{getGetting(user)}</h1>
+    <img src={user.avatar} />
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container = document.getElementById("root");
+
+ReactDOM.render(element, container);
